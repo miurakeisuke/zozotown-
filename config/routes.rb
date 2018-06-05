6533e15b7bda15d6983087a1a54a8b837d381fd2
hirojiang <Jan@Hiromasa-no-MacBook-Air-2.local>
2018-06-05 11:37:34 +0900

    add productController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'products#index'
   resources :users, only: [:index]
+  resources :products, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20180605011618_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      # DBはView作成のため、一時的に作成
+      # genderやnew/usedの記載方法（0,1 or men,women）は後ほど決定する
+      t.string :name, null: false
+      t.text :description
+      t.integer :price, null: false
+      t.integer :point, null: false
+      t.integer :point, null: false
+      t.timestamps
+    end
+  end
+end
