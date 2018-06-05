bfc4c845279e9943ae248feba86517535d1ab1f2
三浦圭亮 <k.miura.rainbow@gmail.com>
2018-06-05 11:36:11 +0900

    Space delete

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -33,8 +33,6 @@ server '52.69.12.5', user: 'ec2-user', roles: %w{app db web}
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
-
-
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
