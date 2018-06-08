fb1074aa1655a5c31b44ecae73d721d6827b4490
三浦圭亮 <k.miura.rainbow@gmail.com>
2018-06-08 11:48:21 +0900

    サイトマップ完成（mixinは一旦保留）
    他のビューとの兼ね合いがあるため、mixinの導入については一旦保留で書いています。

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :products, only: :index
   resources :brands, only: :index
+  resources :sitemaps, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
