c311a01abde12d3019c8181b3e0d92ca0641fa39
三浦圭亮 <k.miura.rainbow@gmail.com>
2018-06-09 15:02:45 +0900

    カート機能事前準備

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :products, only: :index
   resources :brands, only: :index
+  resources :carts, only: [:show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
