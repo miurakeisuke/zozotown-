19716e55e1ceddd1befa7c29bba98e9b75c01e90
taihei22 <taiheipasocon@yahoo.co.jp>
2018-06-11 12:13:38 +0900

    Modifyshowdetails

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+    add_breadcrumb 'ZOZOTOWN', "/"
+    add_breadcrumb 'SHOPNAME', "/"
+    add_breadcrumb 'トップス', "/"
+    add_breadcrumb 'シャツ/ブラウス', "/"
+    add_breadcrumb 'アイテム詳細'
   end
 
 end
