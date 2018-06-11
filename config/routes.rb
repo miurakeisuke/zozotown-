372dc851af217a97ceb0b453c8839c727a1acd8c
taihei22 <taiheipasocon@yahoo.co.jp>
2018-06-11 12:02:36 +0900

    Products-details-all

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :rankings, only: :index
   resources :users, only: [:index]
-  resources :products, only: :index
+  resources :products, only: [:index, :show]
   resources :brands, only: :index
   resources :shops, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
