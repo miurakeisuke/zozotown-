dd38e7f471d1e29878dd162db933d0b50a265cc3
taihei22 <taiheipasocon@yahoo.co.jp>
2018-06-12 17:54:32 +0900

    commentout
    まだ作成していないモデルに対して、コメントアウトを行いました。

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
 class Shop < ApplicationRecord
-  has_many :shop_brand
+  has_many :shop_brands
   has_many :products
-  has_many :shop_news
-  has_many :coupons
-  has_many :brands, through: :shop_brand
+  # has_many :shop_news
+  # has_many :coupons
+  has_many :brands, through: :shop_brands
 end
