c44a56ba747d2c5bc88b407fb58d34ee0dfe0958
taihei22 <taiheipasocon@yahoo.co.jp>
2018-06-12 17:44:15 +0900

    AddModelAssociation
    model(brand,color,image,product,shop_brand,shop,size)

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,6 @@
 class Brand < ApplicationRecord
+  has_many :products
+  has_many :shop_brand
+  has_many :brand_likes
+  has_many :shops, through: :shop_brand
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  has_many :staff_reviews
+  has_many :shop_news
+  has_many :coupons
+  belongs_to :shop
+  belongs_to :category
+  has_many :colors
+  has_many :sizes
+  belongs_to :brand
+  has_many :product_likes
+  has_one :detail
+  has_many :points
+  has_many :orders
+  has_many :images
+  has_many :products_users
+  has_many :users, through: :products_users
 end
