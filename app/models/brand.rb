fbbba5b9fc2083234a8c81810761c3df494b1174
taihei22 <taiheipasocon@yahoo.co.jp>
2018-06-12 18:12:17 +0900

    Rubocopのエラーを修正

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand < ApplicationRecord
-  has_many :products
-  has_many :shop_brands
+  has_many :products, :dependent => :destroy
+  has_many :shop_brands, :dependent => :destroy
   # has_many :brand_likes
   has_many :shops, through: :shop_brands
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ApplicationRecord
-  has_many :shop_brands
-  has_many :products
+  has_many :shop_brands, :dependent => :destroy
+  has_many :products, :dependent => :destroy
   # has_many :shop_news
   # has_many :coupons
   has_many :brands, through: :shop_brands
