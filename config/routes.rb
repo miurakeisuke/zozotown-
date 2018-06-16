1b05fc72cd61b6d286541c8c6b953c5d7577c674
kou777 <ogawakou@ogawahikarinoMacBook-puro.local>
2018-06-16 12:29:25 +0900

    ・綴りの修正 ・ルーティン設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :products, only: :index
   resources :brands, only: :index
+  resources :coordinates, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
