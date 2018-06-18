d3e365beba886d0a36256f0e75a050424f08f62b
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2018-06-18 16:17:18 +0900

    siderの指摘修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index] do
     resources :carts, only: [:show, :update, :destroy]
   end
-  resources :products, only: [:index, :show] do
-    resources :carts, only: [:create]
+  resources :products, only: [:index, :show]
   end
   resources :brands, only: :index
   resources :carts, only: [:show, :update, :destroy]
