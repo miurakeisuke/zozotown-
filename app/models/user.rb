a4a7b80af343b72149f87e8a0b6decff64d4a281
hirojiang <janhiro608@gmail.com>
2018-06-19 19:55:08 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :cart
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :product_likes, :dependent => :destroy
