3ebabff22a5e96951ee7070db1a569af3a7456b7
hirojiang <janhiro608@gmail.com>
2018-06-19 19:39:05 +0900

    user作成時に紐づいたcartテーブルも同時作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   root 'products#index'
   resources :rankings, only: :index
   resources :users, only: [:index] do
