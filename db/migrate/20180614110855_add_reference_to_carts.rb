98dec24c67a4a7c5307ae2cef09e19f6557cee6a
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2018-06-19 18:23:58 +0900

    migrate修正

--- db/migrate/20180614110855_add_reference_to_carts.rb
+++ db/migrate/20180614110855_add_reference_to_carts.rb
@@ -1,5 +1,5 @@
 class AddReferenceToCarts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :carts, :user, foreign_key: true
+    # add_reference :carts, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_030231) do
+ActiveRecord::Schema.define(version: 2018_06_19_092017) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
