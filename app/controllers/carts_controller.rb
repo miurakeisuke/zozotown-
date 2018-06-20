5938cf30a6966d79abc38dc5a83040bcfd345e33
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2018-06-20 12:51:40 +0900

    カート詳細画面_削除ボタン実装＆合計額表示

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,14 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
     @cart_products = @cart.products
+    @product_cart = ProductCart.ids
   end
 
   def update
   end
 
   def destroy
+    ProductCart.find(params[:id]).destroy
+    redirect_to :root
   end
 end
