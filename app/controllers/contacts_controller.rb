d1e0cd6bef40d0dba5a872ccf65421061df24b14
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2018-06-25 16:20:29 +0900

    add_contact_view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,17 @@
 class ContactsController < ApplicationController
+  def create
+  end
 end
+
+
+# class CommentsController < ApplicationController
+#   def create
+#     @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+#     redirect_to "/tweets/#{@comment.tweet.id}"
+#   end
+
+#   private
+#   def comment_params
+#     params.permit(:text, :tweet_id)
+#   end
+# end
