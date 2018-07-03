a3e1a4e42c5becd2dab72815ee49e1ae3d7c5d5b
三浦圭亮 <k.miura.rainbow@gmail.com>
2018-07-03 10:40:17 +0900

    sideci修正

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   resources :product_carts, only: [:create, :update]
   resources :constructions, only: [:index]
   resources :warehouses, only: [:new, :edit, :update, :show, :create]
-  resources :deposits, only: [:index, :new, :edit, :update, :create]
+  resources :deposits, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
